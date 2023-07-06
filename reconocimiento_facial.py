15843cfd2f36828f3f9888979717dee4926d2526
victor toledo <querynetworking@gmail.com>
2023-07-05 23:55:19 -0300

    mostrar caras

--- reconocimiento_facial.py
+++ reconocimiento_facial.py
@@ -2,12 +2,32 @@ import cv2
 import face_recognition as fr
 
 # Cargar Imagenes
-foto_control = fr.load_image_file('FotoD.jpg')
-foto_prueba = fr.load_image_file('FotoA.jpg')
+foto_control = fr.load_image_file('FotoA.jpg')
+foto_prueba = fr.load_image_file('FotoB.jpg')
 
 # Pasar imagenes a rgb
 foto_control = cv2.cvtColor(foto_control, cv2.COLOR_BGR2RGB)
 foto_prueba = cv2.cvtColor(foto_prueba, cv2.COLOR_BGR2RGB)
+
+# Localizar cara control
+lugar_cara_A = fr.face_locations(foto_control)[0]
+cara_codificada_A = fr.face_encodings(foto_control)[0]
+
+lugar_cara_B = fr.face_locations(foto_prueba)[0]
+cara_codificada_B = fr.face_encodings(foto_prueba)[0]
+
+# Mostrar rectangulo
+cv2.rectangle(foto_control,
+              (lugar_cara_A[3], lugar_cara_A[0]),
+              (lugar_cara_A[1], lugar_cara_A[2]),
+              (0, 255, 0),
+              2)
+
+cv2.rectangle(foto_prueba,
+              (lugar_cara_B[3], lugar_cara_B[0]),
+              (lugar_cara_B[1], lugar_cara_B[2]),
+              (0, 255, 0),
+              2)
 
 # Mostrar imagenes
 cv2.imshow('Foto Control', foto_control)
