7e0daeac8c42cfea12a41a089ad893fdb8219bbd
victor toledo <querynetworking@gmail.com>
2023-07-08 02:44:13 -0300

    Registrar asistencias

--- asistencia.py
+++ asistencia.py
@@ -2,6 +2,7 @@ import cv2
 import face_recognition as fr
 import os
 import numpy
+from datetime import datetime
 
 # crear base de datos
 ruta = 'Empleados'
@@ -10,7 +11,7 @@ nombres_empleados = []
 lista_empleados = os.listdir(ruta)
 
 for nombre in lista_empleados:
-    imagen_actual = cv2.imread(f'{ruta}\{nombre}')
+    imagen_actual = cv2.imread(f'{ruta}/{nombre}')
     mis_imagenes.append(imagen_actual)
     nombres_empleados.append(os.path.splitext(nombre)[0])
 
@@ -34,6 +35,20 @@ def codificar(imagenes):
 
     # devolver lista codificada
     return lista_codificada
+
+# Registrar los ingresos
+def registrar_ingresos(persona):
+    f = open('registro.csv', 'r+')
+    lista_datos = f.readlines()
+    nombres_registro = []
+    for linea in lista_datos:
+        ingreso = linea.split(',')
+        nombres_registro.append(ingreso[0])
+
+    if persona not in nombres_registro:
+        ahora = datetime.now()
+        string_ahora = ahora.strftime('%H:%M:%S')
+        f.writelines(f'\n{persona}, {string_ahora}')
 
 lista_empleados_codificada = codificar(mis_imagenes)
 
@@ -74,6 +89,8 @@ else:
             cv2.rectangle(imagen, (x1, y1), (x2, y2), (0, 255, 0), 2)
             cv2.rectangle(imagen, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
             cv2.putText(imagen, nombre, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+
+            registrar_ingresos(nombre)
 
             # Mostrar la imagen obtenida
             cv2.imshow('Imagen web', imagen)
